e809bc5af489c372850d3d971794e27ee47af84b
Rohan Patel <rohanpatel1991@gmail.com>
2015-03-09 14:55:28 -0700

    Revert "Revert "final upload for crawler and extraction""
    This reverts commit b6560cff03ee742c8f3c4c4ebfcde526d65378d0.

--- extractor/src/main/java/edu/calstatela/cs454/instructor/crawler/Main.java
+++ extractor/src/main/java/edu/calstatela/cs454/instructor/crawler/Main.java
@@ -1,5 +1,6 @@
 package edu.calstatela.cs454.instructor.crawler;
 
+import java.awt.Event;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,7 +18,12 @@ import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.BodyContentHandler;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
+import org.codehaus.jackson.map.MappingJsonFactory;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 import org.json.simple.JSONObject;
@@ -35,8 +42,27 @@ import com.google.gson.stream.JsonReader;
 public class Main {
 
 	
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException, SAXException, TikaException {
-		//new Extraction().extract();
+		
+		
+		
+		
+		
+		    
+		/* InputStream in = new FileInputStream("C:/Users/Varun/crawler3-app/"+controlfile);
+			try {
+				for (Iterator it = new ObjectMapper().readValues(
+						new JsonFactory().createJsonParser(in), Map.class); it
+						.hasNext();) {
+					// System.out.println(it.next());
+					LinkedHashMap<String, Object> keyValue = (LinkedHashMap<String, Object>) it.next();
+					//System.out.println(keyValue.get("storage"));*/
+		
+		 
+		
+		
+		
 		 File f = new File("file2.json");
 		 
 		// String c = args[0];
@@ -53,7 +79,9 @@ public class Main {
 		
 		new Extraction().extract();
 	}
+
 }
+	
 
 	
 	
